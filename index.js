a0653faa677857a14dfb31c8f3a6c76d41bd48e0
francisjoy <francisjoy007@gmail.com>
2024-03-02 12:01:45 +0000

    Added helm

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 var LoremIpsum = require('lorem-ipsum').LoremIpsum;
 
 var lorem = new LoremIpsum({
   sentencesPerParagraph: {
-    max: 8,
-    min: 4
+    max: parseInt(process.env.MAX_SENTENCES_PER_PARAGRAPH) || 8,
+    min: parseInt(process.env.MIN_SENTENCES_PER_PARAGRAPH) || 4
   },
   wordsPerSentence: {
-    max: 16,
-    min: 4
+    max: parseInt(process.env.MAX_WORDS_PER_SENTENCE) || 16,
+    min: parseInt(process.env.MIN_WORDS_PER_SENTENCE) || 4
   }
 });
 
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
+app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
